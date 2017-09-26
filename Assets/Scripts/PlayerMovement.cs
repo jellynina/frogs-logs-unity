06dfad8bb424bc1a0d57fd696e8494e7717a69c5
jellynina <jellynina@gmail.com>
2017-09-26 09:40:39 +0800

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Animator playerAnimator;
+	private float moveHorizontal;
+	private float moveVertical;
+	private Vector3 movement;
+
+	// Use this for initialization
+	void Start () {
+		playerAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		moveVertical = Input.GetAxisRaw ("Vertical");
+
+		movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+	}
+
+	void FixedUpdate () {
+		if (movement != Vector3.zero) {
+			playerAnimator.SetFloat ("Speed", 3f);
+		} else {
+			playerAnimator.SetFloat ("Speed", 0F);
+		}
+	}
+}
